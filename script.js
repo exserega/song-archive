76c285e551b3bd6552fddd12aa1dfe34d8291b3e
exserega <153385364+exserega@users.noreply.github.com>
2025-01-14 17:42:08 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const API_KEY = 'Ваш_API_ключ'; // Вставьте сюда ваш API-ключ
-const SHEET_ID = 'Ваш_ID_таблицы'; // Вставьте сюда ID вашей таблицы
+const API_KEY = 'AIzaSyDO2gwifAnZzC3ooJ0A_4vAD76iYakwzlk'; // Вставьте сюда ваш API-ключ
+const SHEET_ID = '1C3gFjj9LAub_Nk9ogqKp3LKpdAxq6j8xlPAsc8OmM5s'; // Вставьте сюда ID вашей таблицы
 const SHEETS = {
     'Быстрые (вертикаль)': 'Быстрые (вертикаль)',
     'Быстрые (горизонталь)': 'Быстрые (горизонталь)',
