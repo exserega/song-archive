9f7777cfc92f59f070a899310f03f8a5f7abaf0d
Agape <153385364+exserega@users.noreply.github.com>
2025-02-24 01:41:51 +0500

    Update script.js

--- script.js
+++ script.js
@@ -754,9 +754,3 @@ document.getElementById('toggle-favorites').addEventListener('click', () => {
         loadGroupPanel(); // Загружаем содержимое панели
     }
 });
-
-
-document.getElementById('toggle-shared-list').addEventListener('click', () => {
-const panel = document.getElementById('shared-list-panel');
-panel.classList.toggle('open');
-});
